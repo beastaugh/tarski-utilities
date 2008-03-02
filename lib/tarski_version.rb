89842b89fa80fc878916249f13ec063ef32ab841
Benedict Eastaugh <benedict@eastaugh.net>
2008-03-02 00:27:23 +0000

    Add some documentation to TarskiVersion

--- lib/tarski_version.rb
+++ lib/tarski_version.rb
@@ -1,14 +1,31 @@
-# Tarski update notifier - Atom feed generator
 require 'rubygems'
 require 'builder'
 
+# The Tarski update notifier: a simple Atom feed generator.
+# 
+# You can use this to create an Atom feed for a dead-simple update notifier
+# which, unlike the built-in WordPress update notifier, requires no data
+# from the client, since all it does is check the version listed in the feed
+# and match it against its own version.
 class TarskiVersion
   
+  # Version data is passed to the constructor so it's not too tightly coupled
+  # with the data collection method.
+  # 
+  # It just uses the current time so if your client cares about that, you might
+  # want to change it so the date and time are specified in the version data.
   def initialize(version_data)
     @version_data = version_data
     @time = DateTime.now.to_s
   end
-    
+  
+  # Writes an Atom feed encapsulating the version data to the target location.
+  # 
+  # Currently it only does one--i.e. the latest--entry. This is just a
+  # limitation of my simplistic config file; adding support for multiple
+  # entries would be trivial.
+  # 
+  # TODO: Move some of the configuration details out of the class.
   def publish_feed(target)
     @file = File.new(target, "w+")
     xml = Builder::XmlMarkup.new(:target => @file, :indent => 2)
